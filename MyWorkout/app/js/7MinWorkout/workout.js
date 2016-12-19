cd635561e582b2d85cfaac444d8abc2ce2b92ba7
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2016-12-20 00:19:30 +0100

    added sound controller

--- MyWorkout/app/js/7MinWorkout/workout.js
+++ MyWorkout/app/js/7MinWorkout/workout.js
@@ -231,3 +231,50 @@ angular.module('7minWorkout').controller('WorkoutController',
     init();
 
     }]);
+
+/**
+ * Kontroler dzwięku
+ */
+angular.module('7minWorkout').controller('WorkoutAudioController',
+    ['$scope', '$timeout',
+        function ($scope, $timeout) {
+            $scope.exercisesAudio = [];
+            var workoutPlanwatch = $scope.$watch('workoutPlan',
+                function (newValue, oldValue) {
+                    if (newValue) { // newValue==workoutPlan
+                        angular.forEach($scope.workoutPlan.exercises,
+                            function (exercise) {
+                                $scope.exercisesAudio.push({
+                                    src: exercise.details.nameSound,
+                                    type: "audio/wav"
+                                });
+                            });
+                        workoutPlanwatch(); // przerywamy obserwację zmian właściwości
+                    }
+                });
+            $scope.$watch('currentExercise', function (newValue, oldValue) {
+                if (newValue && newValue != oldValue) {
+                    if ($scope.currentExercise.details.name == 'rest') {
+                        $timeout(function () {
+                            $scope.nextUpAudio.play();
+                        }, 2000);
+                        $timeout(function () {
+                            $scope.nextUpExerciseAudio.play($scope.currentExerciseIndex + 1, true);
+                        }, 3000);
+                    }
+                }
+            });
+            $scope.$watch('currentExerciseDuration', function (newValue, oldValue) {
+                if (newValue) {
+                    if (newValue == $scope.currentExercise.duration / 2 && $scope.currentExercise.details.name != 'rest') {
+                        $scope.halfWayAudio.play();
+                    }
+                else if (newValue == $scope.currentExercise.duration - 3) {
+                        $scope.aboutToCompleteAudio.play();
+                    }
+                }
+            });
+            var init = function () {
+            }
+        init();
+    }]);
